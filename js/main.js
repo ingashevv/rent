a24cc775b9e70274d5aed0e913e136a9985d0382
Daud <ingashevdaudino@gmail.com>
2023-07-05 16:32:52 +0300

    full project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg"></button>',
+    })
+
+});
+
+
+
